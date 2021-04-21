0e3f60820bc0f872dfb9ee5d1ee3bcb0a5ac421f
Tortique <adrien86190@yahoo.fr>
2021-04-21 16:37:59 +0200

    #6 CRUD Trade

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/TradeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/TradeController.java
@@ -1,6 +1,8 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.repositories.TradeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +13,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.sql.Timestamp;
+
+/**
+ * Created by SERVANT Adrien
+ * Date 04/21/21
+ * Time 16:35
+ *
+ * Comment : Html's problems "can't resolve [...]" worked fine, its known issues since 2015 with Intellij
+ */
 
 @Controller
 @EnableWebSecurity
 public class TradeController {
-    // TODO: Inject Trade service
+    @Autowired
+    private TradeRepository tradeRepository;
 
     @RequestMapping("/trade/list")
     public String home(Model model)
     {
-        // TODO: find all Trade, add to model
+        model.addAttribute("trades", tradeRepository.findAll());
         return "trade/list";
     }
 
@@ -31,26 +43,39 @@ public class TradeController {
 
     @PostMapping("/trade/validate")
     public String validate(@Valid Trade trade, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Trade list
+        if (!result.hasErrors()) {
+            trade.setCreationDate(new Timestamp(System.currentTimeMillis()));
+            tradeRepository.save(trade);
+            model.addAttribute("trades", tradeRepository.findAll());
+            return "redirect:/trade/list";
+        }
         return "trade/add";
     }
 
     @GetMapping("/trade/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Trade by Id and to model then show to the form
+        Trade trade = tradeRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid trade Id:" + id));
+        model.addAttribute("trade", trade);
         return "trade/update";
     }
 
     @PostMapping("/trade/update/{id}")
     public String updateTrade(@PathVariable("id") Integer id, @Valid Trade trade,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Trade and return Trade list
+        if (result.hasErrors()) {
+            trade.setTradeId(id);
+            return "trade/update";
+        }
+        tradeRepository.save(trade);
+        model.addAttribute("trades", tradeRepository.findAll());
         return "redirect:/trade/list";
     }
 
     @GetMapping("/trade/delete/{id}")
     public String deleteTrade(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Trade by Id and delete the Trade, return to Trade list
+        Trade trade = tradeRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid trade Id:" + id));
+        tradeRepository.delete(trade);
+        model.addAttribute("trades", tradeRepository.findAll());
         return "redirect:/trade/list";
     }
 }
